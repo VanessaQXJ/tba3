20aba9f30efc580685d1f4535d172102dfa3670e
vanessa.1112@yahoo.com <vanessa.1112@yahoo.com>
2016-11-20 13:26:05 -0800

    Hi david,I am not clear how to do the accessory cards, so i deleted the one i did before and followed what you edited in the execute.java to do it. can you try on ur eclipse to see if this works?

--- TheBlackArts/src/core/Accessory.java
+++ TheBlackArts/src/core/Accessory.java
@@ -1,70 +1,54 @@
 package core;
-import java.util.ArrayList; 
+
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Accessory extends Card {
-	private int loseHP = 1;
-	private int gainHP = 1;
-	private boolean isUsed = false;
-	Scanner input = new Scanner(System.in);
-	public Accessory() {
-	 
+	
+public Accessory(String name, int goldCost) {
+		super.setGoldCost(goldCost);
 	}
+    public void removeACard(Game game){
+    	public void removeACard(ArrayList<Card> inPlayZone, ArrayList<Card> deadZone){
+        	Scanner input = new Scanner(System.in);
+    		Card cards;
 
-	public Accessory (int loseHP, int gainHP){
-		this.loseHP = loseHP;
-		this.gainHP = gainHP;
-	}
-    public Accessory (String name, int loseHP, int gainHP, int goldCost) {
-    	super.setCardName(name);
-    	this.loseHP = loseHP;
-		this.gainHP = gainHP;
-        super.setGoldCost(goldCost);
-        
+        	if(game.getTotalTurns() % 2 == 0){
+        		ArrayList<Card> playerTwoInPlayZone = game.getPlayerTwoInPlayZone();
+        		System.out.println("select a CARD you want to remove: ");
+        		
+        		for (int i = 0, n = playerTwoInPlayZone.size(); i < n; i++) {
+    					System.out.println(playerTwoInPlayZone.get(i).getCardName());
+        		}
+        		int selectCard = input.nextInt() - 1;
+            	
+            	cards = PlayerTwoInPlayZone.get(selectCard);
+            	PlayerTwoInPlayZone.remove(selectCard);
+            	deadZone.add(cards);
+        		
+        	}
+        	
+        	else{
+        		
+        		for (int i = 0, n = inPlayZone.size(); i < n; i++) {
+                    	System.out.println("Select a card to remove ");
+        		}
+        		
+        		for (int j = 0; j < n; j++){
+                     System.out.println((j + 1) + ": " + inPlayZone.get(j).getCardName());
+                     }
+                    	
+        			int selectCard = input.nextInt() - 1;
+                	
+                	cards = inPlayZone.get(selectCard);
+                	inPlayZone.remove(selectCard);
+                	deadZone.add(cards);
+        	}
+        	}
     }
-    //
-    
-    public void setgainHP(int gainHP){
-    	this.gainHP = gainHP;
-    }
-    public int getgainHP(){
-    	return this.gainHP;	
-    }
-    //
-    public void setloseHP(int loseHP){
-    	this.loseHP = loseHP;
-    }
-    public int getloseHP(){
-    	return this.loseHP;	
-    }
-    //
-    
-    public void AccessoryCard(){
-    	ArrayList<Card> inPlayZone = new ArrayList<Card>();
-    	for(Card card: inPlayZone){
-    		System.out.println(" "+card.getCardName());
-    	}
-    	if(isUsed){
-    		System.out.println("select a CARD from your enemy: ");
-			int s = input.nextInt();
+}
 
-			if(s>inPlayZone.size()){
-				System.out.println("Please enter a valid number: ");
-				    	s = input.nextInt();
-				    }
-
-			else{
-				Card card = inPlayZone.get(s-1);
-		    	if(card instanceof Monster){
-		    		inPlayZone.remove(s);
-		    		System.out.println("You removed a Monster from your enemy");
-		    	}
-		    	if(card instanceof Action){
-		    		inPlayZone.remove(s);
-		    		System.out.println("You removed an Action card from your enemy");
-		    	}
-			}
-			}
-    	}
-    }
     
+
+    
+   
 
---
+++ TheBlackArts/src/test/TestAccessory.java
@@ -0,0 +1,24 @@
+package test;
+import java.util.ArrayList;
+import core.Accessory;
+import core.Card;
+
+public class TestAccessory {
+	public static void main(String[] args){
+		ArrayList<Card> inPlayZone = new ArrayList<Card>();
+		ArrayList<Card> deadZone = new ArrayList<Card>();
+
+		inPlayZone.add(new Card());
+		inPlayZone.add(new Card());
+	
+		System.out.println(inPlayZone);
+		System.out.println(deadZone);
+		
+		Accessory accessory = new Accessory ("Ancient Tomb",2);
+		accessory.removeACard(inPlayZone, deadZone);
+		System.out.println(inPlayZone);
+		System.out.println(deadZone);
+	}
+	
+//
+}
